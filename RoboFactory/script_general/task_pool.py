02340f6b4564707c80bad770bcaeed37e58a6b76
IranQin <2320994734@qq.com>
2025-04-22 11:17:29 +0800

    update assets config

--- RoboFactory/script_general/task_pool.py
+++ RoboFactory/script_general/task_pool.py
@@ -19,7 +19,7 @@ TASK_POOL = [
         "task_id": "1-1",
         "task_name": "move_asset_to_target",
         "description": "Move the <mask1> to the <mask2>",
-        "mask1": ["plate", "cup", "book", "pan"],
+        "mask1": ["plate", "cup"],
         "mask2": ["dining table", "counter", "sink area", "stove"],
         "robot_roles": ["humanoid"],
         "ground_truth": [
@@ -41,7 +41,7 @@ TASK_POOL = [
         "task_id": "1-2",
         "task_name": "move_asset_to_target",
         "description": "Move the <mask1> to the <mask2>",
-        "mask1": ["plate", "cup", "book", "pan"],
+        "mask1": ["plate", "cup"],
         "mask2": ["dining table", "counter", "sink area", "stove"],
         "robot_roles": ["wheeled"],
         "ground_truth": [
@@ -59,31 +59,31 @@ TASK_POOL = [
             },
         ], 
     },
-    {
-        "task_id": "1-3",
-        "task_name": "move_asset_to_target",
-        "description": "Move the <mask1> to the <mask2>",
-        "mask1": ["bottle", "screwdriver", "box", "tool"],
-        "mask2": ["kitchen counter", "toolbox", "sink", "shelf"],
-        "robot_roles": ["arm", "dog", "arm"],
-        "ground_truth": [
-            {"R1": ["Move", "<mask1>"],    "R2": ["Move", "R1"]},
-            {"R1": ["Reach", "<mask1>"]},
-            {"R1": ["Grasp", "<mask1>"]},
-            {"R1": ["Handover", "R2"]},
-            {"R2": ["Move", "R3"]},
-            {"R3": ["Reach", "<mask1>"]},
-            {"R3": ["Grasp", "<mask1>"]},
-            {"R3": ["Place", "<mask2>"]}
-        ],
-        "init_pos": [
-            {
-                "name_key": "mask1",
-                "pos": ["dining table", "kitchen counter"],
-                "exclude_keys" : ["mask2"]
-            },
-        ], 
-    },
+    # {
+    #     "task_id": "1-3",
+    #     "task_name": "move_asset_to_target",
+    #     "description": "Move the <mask1> to the <mask2>",
+    #     "mask1": ["bottle", "screwdriver", "box", "tool"],
+    #     "mask2": ["kitchen counter", "toolbox", "sink", "shelf"],
+    #     "robot_roles": ["arm", "dog", "arm"],
+    #     "ground_truth": [
+    #         {"R1": ["Move", "<mask1>"],    "R2": ["Move", "R1"]},
+    #         {"R1": ["Reach", "<mask1>"]},
+    #         {"R1": ["Grasp", "<mask1>"]},
+    #         {"R1": ["Handover", "R2"]},
+    #         {"R2": ["Move", "R3"]},
+    #         {"R3": ["Reach", "<mask1>"]},
+    #         {"R3": ["Grasp", "<mask1>"]},
+    #         {"R3": ["Place", "<mask2>"]}
+    #     ],
+    #     "init_pos": [
+    #         {
+    #             "name_key": "mask1",
+    #             "pos": ["dining table", "kitchen counter"],
+    #             "exclude_keys" : ["mask2"]
+    #         },
+    #     ], 
+    # },
     {
         "task_id": "2-1",
         "task_name": "parallel_dual_asset_to_plate",
@@ -116,38 +116,38 @@ TASK_POOL = [
             },
         ], 
     },
-    {
-        "task_id": "2-2",
-        "task_name": "parallel_dual_asset_to_plate",
-        "description": "Transport the <mask1> and the <mask2> into the <mask3> together",
-        "mask1": ["apple", "tomato", "bun"],
-        "mask2": ["banana", "pear", "peach"],
-        "mask3": ["plate", "bowl", "tray"],
-        "robot_roles": ["wheeled", "humanoid"],
-        "ground_truth": [
-            {"R1": ["Move", "<mask1>"],      "R2": ["Move", "<mask2>"]},
-            {"R1": ["Reach", "<mask1>"],     "R2": ["Reach", "<mask2>"]},
-            {"R1": ["Grasp", "<mask1>"],     "R2": ["Grasp", "<mask2>"]},
-            {"R1": ["Move", "<mask3>"],      "R2": ["Move", "<mask3>"]},
-            {"R1": ["Place", "<mask3>"],     "R2": ["Place", "<mask3>"]}
-        ],
-        "init_pos": [
-            {
-                "name_key": "mask1",
-                "pos": ["dining table", "kitchen counter"],
-                "exclude_keys" : ["mask3"]
-            },
-            {
-                "name_key": "mask2",
-                "pos": ["dining table", "kitchen counter"],
-                "exclude_keys" : ["mask3"]
-            },
-            {
-                "name_key": "mask3",
-                "pos": ["dining table", "kitchen counter"],
-            },
-        ]
-    },
+    # {
+    #     "task_id": "2-2",
+    #     "task_name": "parallel_dual_asset_to_plate",
+    #     "description": "Transport the <mask1> and the <mask2> into the <mask3> together",
+    #     "mask1": ["apple", "tomato", "bun"],
+    #     "mask2": ["banana", "pear", "peach"],
+    #     "mask3": ["plate", "bowl", "tray"],
+    #     "robot_roles": ["wheeled", "humanoid"],
+    #     "ground_truth": [
+    #         {"R1": ["Move", "<mask1>"],      "R2": ["Move", "<mask2>"]},
+    #         {"R1": ["Reach", "<mask1>"],     "R2": ["Reach", "<mask2>"]},
+    #         {"R1": ["Grasp", "<mask1>"],     "R2": ["Grasp", "<mask2>"]},
+    #         {"R1": ["Move", "<mask3>"],      "R2": ["Move", "<mask3>"]},
+    #         {"R1": ["Place", "<mask3>"],     "R2": ["Place", "<mask3>"]}
+    #     ],
+    #     "init_pos": [
+    #         {
+    #             "name_key": "mask1",
+    #             "pos": ["dining table", "kitchen counter"],
+    #             "exclude_keys" : ["mask3"]
+    #         },
+    #         {
+    #             "name_key": "mask2",
+    #             "pos": ["dining table", "kitchen counter"],
+    #             "exclude_keys" : ["mask3"]
+    #         },
+    #         {
+    #             "name_key": "mask3",
+    #             "pos": ["dining table", "kitchen counter"],
+    #         },
+    #     ]
+    # },
     {
         "task_id": "3-1",
         "task_name": "set_plate_and_fork_on_table",
