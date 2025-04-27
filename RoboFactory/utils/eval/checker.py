d8cc8b9373f5e5d2d803e5fe81f9ff8d9c0eb3ae
IranQin <2320994734@qq.com>
2025-04-27 18:16:46 +0800

    update container check

--- RoboFactory/utils/eval/checker.py
+++ RoboFactory/utils/eval/checker.py
@@ -80,9 +80,9 @@ class Checker:
         elif operation_name == 'place':
             return self.check_target_aligned_position(params[0], params[1], assets, agents) and len(params[0].get_carried_objects()) > 0
         elif operation_name == 'open':
-            return self.check_agent_relative_position(params[0], params[1]) and self.check_agent_has_free_end_effector(params[0]) and self.check_pos_is_isolated(params[1].container_position)
+            return self.check_agent_relative_position(params[0], params[1]) and self.check_agent_has_free_end_effector(params[0]) and hasattr(params[1], 'container_position') and self.check_pos_is_isolated(params[1].container_position)
         elif operation_name == 'close':
-            return self.check_agent_relative_position(params[0], params[1]) and self.check_agent_has_free_end_effector(params[0]) and not self.check_pos_is_isolated(params[1].container_position)
+            return self.check_agent_relative_position(params[0], params[1]) and self.check_agent_has_free_end_effector(params[0]) and hasattr(params[1], 'container_position') and not self.check_pos_is_isolated(params[1].container_position)
         elif operation_name == 'handover':    # handover <asset, agent>
             return self.check_agent_relative_position(params[0], params[2]) and len(params[0].get_carried_objects()) > 0 and self.check_agent_has_free_end_effector(params[2])
         elif operation_name == 'interact':    # known: agent may activate irrelevant assets, can be solved by informing each task of interact scopes
