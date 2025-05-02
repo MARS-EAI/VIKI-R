14f586491d498ed56af70fa3968af73d86214b7d
kangli <2320994734@qq.com>
2025-05-03 01:05:47 +0800

    fix temporal constraint check

--- RoboFactory/utils/eval/eval.py
+++ RoboFactory/utils/eval/eval.py
@@ -227,9 +227,6 @@ class Eval:
                 for temporal_status in temporal_constraint:
                     if self.check_constraint(temporal_status):
                         if not satisfied_temporal_status:
-                            print(command)
-                            print(self.env.assets['apple'].pos.name)
-                            print(self.env.assets['knife'].is_activated)
                             self.error_desc_code = 'FAILED_TEMPORAL_CONSTRAINT'
                             return False
                     else:
